562a22f5ba304b62afbeb85ff5b9d1352b0f6845
krasjbee <zloyjb@gmail.com>
2021-06-01 16:47:29 +0700

    feat: entered numbers check is added

--- app/src/main/java/com/rsschool/android2021/FirstFragment.kt
+++ app/src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -1,7 +1,6 @@
 package com.rsschool.android2021
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,17 +8,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import java.lang.NumberFormatException
-import kotlin.math.max
+
 
 class FirstFragment : Fragment() {
 
     private var generateButton: Button? = null
     private var previousResult: TextView? = null
-    private var router:Router?=null
-    private var etMin:EditText?=null
-    private var etMax:EditText?=null
-
+    private var router: Router? = null
+    private var etMin: EditText? = null
+    private var etMax: EditText? = null
 
 
     override fun onCreateView(
@@ -34,24 +31,43 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         previousResult = view.findViewById(R.id.previous_result)
         generateButton = view.findViewById(R.id.generate)
-        etMin=view.findViewById(R.id.min_value)
-        etMax=view.findViewById(R.id.max_value)
+        etMin = view.findViewById(R.id.min_value)
+        etMax = view.findViewById(R.id.max_value)
 
-        if(context is Router){
-            router=context as Router
+
+        if (context is Router) {
+            router = context as Router
         }
+
 
         val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
         previousResult?.text = "Previous result: ${result.toString()}"
 
-        var minNumber:Int
-
-
 
         generateButton?.setOnClickListener {
-            val minString = etMin?.text.toString().toInt()
-            val maxString = etMax?.text.toString().toInt()
-            router?.toSecondFragment(minString,maxString)
+            val minString = etMin?.text.toString()
+            val maxString = etMax?.text.toString()
+            if (isNumbersAllowed(minString, maxString)) {
+                router?.toSecondFragment(minString.toInt(), maxString.toInt())
+            }
+        }
+    }
+
+    /**
+     * Checks if input numbers is in integer range , or if min is greater than max
+     */
+    private fun isNumbersAllowed(min: String, max: String): Boolean {
+        return when {
+            min.toIntOrNull() == null -> false.also {
+                etMin?.error = "Number must be in integer range "
+            }
+            max.toIntOrNull() == null -> false.also {
+                etMax?.error = "Number must be in integer range "
+            }
+            min.toInt() > max.toInt() -> false.also {
+                etMax?.error = "Max number must be greater than min"
+            }
+            else -> true
         }
     }
 
--- app/src/main/java/com/rsschool/android2021/SecondFragment.kt
+++ app/src/main/java/com/rsschool/android2021/SecondFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import kotlin.random.Random
 
 class SecondFragment : Fragment() {
 
